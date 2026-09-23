96c9c017349505168c3735cb364400747504e2f9
gaellend <gaelle.industri@gmail.com>
2026-09-23 12:37:22 +0200

    dependencies + update kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -51,4 +52,19 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation(libs.ktor.core)
+    implementation(libs.ktor.engine)
+    implementation(libs.ktor.log)
+    implementation(libs.ktor.content)
+    implementation(libs.ktor.serialization)
+
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.ktor3)
+
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
+
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 }
